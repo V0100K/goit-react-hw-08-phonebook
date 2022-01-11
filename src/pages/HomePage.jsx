7464a972fdd54634131bb4d164cf94986c80f50e
Vitaliy  Malyshko <malyshko2001@gmail.com>
2022-01-12 00:57:15 +0200

    replace HTML file

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import style from './HomePage.module.css'
 const HomePage = () => (
     <div className={style.container}>
         <h1 className={style.title}>
-            Contacts Book{' '}
+            Contacts Book
         </h1>
     </div>
 );
